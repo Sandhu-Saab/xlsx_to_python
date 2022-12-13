dae74b8f468c62937fa4dcc2fa9637c860dde009
Sharandeep Singh <79913163+Sandhu-Saab@users.noreply.github.com>
2022-12-13 12:47:53 +0530

    Update xlsx_to_python.py

--- xlsx_to_python.py
+++ xlsx_to_python.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 import psycopg2
 
-engine = create_engine("postgresql+psycopg2://crm:root@crm.centralus.cloudapp.azure.com:5432/crm", pool_size=100000, max_overflow=200000000)
+engine = create_engine("postgresql+psycopg2://USERNAME:PASSWORD@localhost:PORT/DATABASE", pool_size=100000, max_overflow=200000000)
 
 with pd.ExcelFile('C:/Users/dakus/OneDrive/Desktop/trainer_details.xlsx') as xls:
     df=pd.read_excel(xls)
