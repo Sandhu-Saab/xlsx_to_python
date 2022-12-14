de94c30b0e0dc7fe34ec441f5df833f0f374d3a0
Sharandeep Singh <79913163+Sandhu-Saab@users.noreply.github.com>
2022-12-14 12:58:50 +0530

    Update xlsx_to_python.py

--- xlsx_to_python.py
+++ xlsx_to_python.py
@@ -2,9 +2,10 @@ import pandas as pd
 from sqlalchemy import create_engine
 import psycopg2
 
-engine = create_engine("postgresql+psycopg2://USERNAME:PASSWORD@localhost:PORT/DATABASE", pool_size=100000, max_overflow=200000000)
+engine = create_engine("postgresql+psycopg2://crm:root@localhost:5432/crm", pool_size=100000, max_overflow=200000000)
 
-with pd.ExcelFile('C:/Users/dakus/OneDrive/Desktop/trainer_details.xlsx') as xls:
+
+with pd.ExcelFile('/home/azureuser/xlsx_to_python/leadsdta.xlsx') as xls:
     df=pd.read_excel(xls)
     df.to_sql(name='portalapp_trainer',con=engine, if_exists='append',index=False)
     
